26fe23c7ad9d35490717eaf7afb75c76fb42dcc4
miguegutierrez <miguelonmelon92@gmali.com>
2023-08-16 20:49:38 -0300

    solucion del click

--- eventos.js
+++ eventos.js
@@ -1,2 +1,17 @@
-const divElement = document.querySelector("div");
-divElement.addEventListener("click", function(){ alert("Hola! Soy el div"); });
+//referencias al div y al botón
+const div = document.getElementById("miDiv");
+const boton = document.getElementById("botonSaludar");
+
+//manejador de click para el botón
+boton.addEventListener('click', (event) => {
+
+    event.stopPropagation();
+
+    alert('Hola!');
+});
+
+//manejador de click para el div
+div.addEventListener('click', () => {
+
+    alert('Hola!, Soy el div');
+});
